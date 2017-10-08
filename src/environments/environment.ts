2f955feb4f16aea544a56816239b1b9fa9d06589
Ben Lynn Vineyard IV <ben.vineyard@gmail.com>
2017-10-08 02:41:30 -0700

    adding dev to environment

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -8,13 +8,24 @@
 // `ng build --env=prod` then `environment.prod.ts` will be used instead.
 // The list of which env maps to which file can be found in `.angular-cli.json`.
 
+// LOCAL
+// export const environment = {
+//   production: false,
+//   productLink: 'http://SayNoToLines.com',
+//   checkInSvcRemoteUrl: 'http://localhost:3000/v1/buffet/reservation',
+//   completeSingleReservationSvcRemoteUrl: 'https://localhost:3000/v1/buffet/completesingle',
+//   getReservationQueueSvcRemoteUrl: 'https://localhost:3000/v1/buffet/peek',
+//   finalizeReservationsSvcRemoteUrl: 'https://localhost:3000/v1/buffet/complete',
+//   mgmBuffetGuestsListName: `mgmBuffetGuests`,
+//   mgmBuffetGuestsGetQueueCount: 10,
+// };
+
+// DEV
 export const environment = {
-  production: false,
-  productLink: 'http://SayNoToLines.com',
-  checkInSvcRemoteUrl: 'http://localhost:3000/v1/buffet/reservation',
-  completeSingleReservationSvcRemoteUrl: 'https://localhost:3000/v1/buffet/completesingle',
-  getReservationQueueSvcRemoteUrl: 'https://localhost:3000/v1/buffet/peek',
-  finalizeReservationsSvcRemoteUrl: 'https://localhost:3000/v1/buffet/complete',
+  checkInSvcRemoteUrl: 'https://botvana-api-reservation.herokuapp.com/v1/buffet/reservation',
+  completeSingleReservationSvcRemoteUrl: 'https://botvana-api-reservation.herokuapp.com/v1/buffet/completesingle',
+  getReservationQueueSvcRemoteUrl: 'https://botvana-api-reservation.herokuapp.com/v1/buffet/peek',
+  finalizeReservationsSvcRemoteUrl: 'https://botvana-api-reservation.herokuapp.com/v1/buffet/complete',
   mgmBuffetGuestsListName: `mgmBuffetGuests`,
   mgmBuffetGuestsGetQueueCount: 10,
-};
+}
