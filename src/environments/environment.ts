0a808589b584a85450639512acd71249a35cce95
Ben Lynn Vineyard IV <ben.vineyard@gmail.com>
2017-10-08 02:44:58 -0700

    fixing dev environment settings

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -22,6 +22,8 @@
 
 // DEV
 export const environment = {
+  production: false,
+  productLink: 'http://SayNoToLines.com',
   checkInSvcRemoteUrl: 'https://botvana-api-reservation.herokuapp.com/v1/buffet/reservation',
   completeSingleReservationSvcRemoteUrl: 'https://botvana-api-reservation.herokuapp.com/v1/buffet/completesingle',
   getReservationQueueSvcRemoteUrl: 'https://botvana-api-reservation.herokuapp.com/v1/buffet/peek',
