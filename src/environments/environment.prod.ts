31c61f01922c997ffc411474b18d563934523050
Ben Lynn Vineyard IV <ben.vineyard@gmail.com>
2017-10-16 21:46:46 -0700

    Updating svc urls

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -6,10 +6,10 @@
 export const environment = {
   production: true,
   productLink: 'http://SayNoToLines.com',
-  checkInSvcRemoteUrl: 'https://botvana-api-reservation.herokuapp.com/v1/buffet/reservation',
-  completeSingleReservationSvcRemoteUrl: 'https://botvana-api-reservation.herokuapp.com/v1/buffet/completesingle',
-  getReservationQueueSvcRemoteUrl: 'https://botvana-api-reservation.herokuapp.com/v1/buffet/peek',
-  finalizeReservationsSvcRemoteUrl: 'https://botvana-api-reservation.herokuapp.com/v1/buffet/complete',
+  checkInSvcRemoteUrl: 'https://reservations.saynotolines.com/v1/buffet/reservation',
+  completeSingleReservationSvcRemoteUrl: 'https://reservations.saynotolines.com/v1/buffet/completesingle',
+  getReservationQueueSvcRemoteUrl: 'https://reservations.saynotolines.com/v1/buffet/peek',
+  finalizeReservationsSvcRemoteUrl: 'https://reservations.saynotolines.com/v1/buffet/complete',
   mgmBuffetGuestsListName: `mgmBuffetGuests`,
   mgmBuffetGuestsGetQueueCount: 10,
 };
