b5ed0ed46f650fc4228d151fd6e82ff26e5a88d8
Ben Lynn Vineyard IV <ben.vineyard@gmail.com>
2017-10-16 21:48:27 -0700

    Updates

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -6,10 +6,10 @@
 export const environment = {
   production: true,
   productLink: 'http://SayNoToLines.com',
-  checkInSvcRemoteUrl: 'https://reservations.saynotolines.com/v1/buffet/reservation',
-  completeSingleReservationSvcRemoteUrl: 'https://reservations.saynotolines.com/v1/buffet/completesingle',
-  getReservationQueueSvcRemoteUrl: 'https://reservations.saynotolines.com/v1/buffet/peek',
-  finalizeReservationsSvcRemoteUrl: 'https://reservations.saynotolines.com/v1/buffet/complete',
+  checkInSvcRemoteUrl: 'http://reservations.saynotolines.com/v1/buffet/reservation',
+  completeSingleReservationSvcRemoteUrl: 'http://reservations.saynotolines.com/v1/buffet/completesingle',
+  getReservationQueueSvcRemoteUrl: 'http://reservations.saynotolines.com/v1/buffet/peek',
+  finalizeReservationsSvcRemoteUrl: 'http://reservations.saynotolines.com/v1/buffet/complete',
   mgmBuffetGuestsListName: `mgmBuffetGuests`,
   mgmBuffetGuestsGetQueueCount: 10,
 };
